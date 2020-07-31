474b2b91d672aa1f9e754ce07f10f71144fab000
JosephLisk93 <josephlisk13@gmail.com>
2020-07-31 16:58:25 -0500

    Updates

---
+++ precache-manifest.00ce436b535a40567a4d21ff3ff59caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bb582a57b207420d6f7136a278c43f",
+    "url": "/JosephLisk93/sp-react-deploy/index.html"
+  },
+  {
+    "revision": "3fe286e0e1595b53f7e5",
+    "url": "/JosephLisk93/sp-react-deploy/static/css/2.ed590494.chunk.css"
+  },
+  {
+    "revision": "ecb4d83a07199727549c",
+    "url": "/JosephLisk93/sp-react-deploy/static/css/main.3ef124bd.chunk.css"
+  },
+  {
+    "revision": "3fe286e0e1595b53f7e5",
+    "url": "/JosephLisk93/sp-react-deploy/static/js/2.4c43e694.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/JosephLisk93/sp-react-deploy/static/js/2.4c43e694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb4d83a07199727549c",
+    "url": "/JosephLisk93/sp-react-deploy/static/js/main.9546e557.chunk.js"
+  },
+  {
+    "revision": "ca9517bd7d8493b298a9",
+    "url": "/JosephLisk93/sp-react-deploy/static/js/runtime-main.7318da60.js"
+  }
+]);
